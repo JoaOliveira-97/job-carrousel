c6638847de8e1ad5819da71c3e6517983e4d0bb4
João Oliveira <joao.ooliveirac@gmail.com>
2025-01-14 16:33:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector('.jobOne').addEventListener('mouseover', function() {
+    document.getElementById('b1').style.backgroundColor = 'rgb(233, 166, 85)';
+});
+
+document.querySelector('.jobOne').addEventListener('mouseout', function() {
+    document.getElementById('b1').style.backgroundColor = 'lightgray';
+});
+
+document.querySelector('.jobTwo').addEventListener('mouseover', function() {
+    document.getElementById('b2').style.backgroundColor = 'rgb(233, 166, 85)';
+});
+
+document.querySelector('.jobTwo').addEventListener('mouseout', function() {
+    document.getElementById('b2').style.backgroundColor = 'lightgray';
+});
+
+document.querySelector('.jobThree').addEventListener('mouseover', function() {
+    document.getElementById('b3').style.backgroundColor = 'rgb(233, 166, 85)';
+});
+
+document.querySelector('.jobThree').addEventListener('mouseout', function() {
+    document.getElementById('b3').style.backgroundColor = 'lightgray';
+});
